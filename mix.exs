791fbf5d3b89d0784b245057c3cb922815aafbeb
Samuel Ventura <samuel.ventura@yeico.com>
2026-01-22 23:25:31 -0600

    Initial commit.

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemRpi3.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi3
+  @github_organization "YeicoCom"
+  @app :yeico_firmware_rpi3
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
